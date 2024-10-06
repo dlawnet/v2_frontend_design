3f3103b2f3effad74faf7ec1579c6b04dc3e0997
devlekan <dev.lekan@buckslaw.net>
2024-10-06 12:44:09 -0700

    updating ...

--- src/components/constant/NotFound.jsx
+++ src/components/constant/NotFound.jsx
@@ -7,13 +7,13 @@ function NotFound() {
     navigate("/");
   }
   return (
-    <div>
+    <div className="flex-col justify-center items-center text-[var(--secondary-color)] flex space-y-3 bg-[var(--primary-color)] h-dvh">
       <h1>404: Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
 
       <button
         onClick={handleHome}
-        className="buttonWhite uppercase hover:text-black bg-black hover:border hover:border-stone-500 outline-none flex text-white lg:py-3 lg:px-5 py-2 px-4 rounded-full"
+        className="buttonWhite uppercase hover:text-black bg-[var(--secondary-color)] hover:border hover:border-stone-500 outline-none text-[var(--primary-color)] font-semibold flex lg:px-5 py-2 px-4 rounded-full"
       >
         Home
       </button>
