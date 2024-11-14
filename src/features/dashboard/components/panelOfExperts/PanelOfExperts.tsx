256287c41b460d962a066037a7385aedd7e44623
DEENOluwatobi <shodimutobie@gmail.com>
2024-11-14 14:47:35 +0100

    POE page

--- src/features/dashboard/components/panelOfExperts/PanelOfExperts.tsx
+++ src/features/dashboard/components/panelOfExperts/PanelOfExperts.tsx
@@ -32,102 +32,108 @@ const PanelOfExperts = () => {
   });
 
   return (
-    <div className="mt-5 px-10 lg:px-0 lg:pr-5 flex flex-col items-center">
+    <div className="w-full  flex flex-col gap-2 md:gap-6 p-6">
       <div className="max-w-full w-full lg:max-w-[893px]">
-        <h3 className="text-3xl lg:text-2xl">Panel of Experts</h3>
-        <p className="text-base sm:text-lg mt-1">
+        <h3 className="text-lg lg:text-2xl">Panel of Experts</h3>
+        <p className="text-sm sm:text-base mt-1">
           Select a Service Type from our panel of experts.
         </p>
       </div>
-      <div className="flex pb-5 justify-center flex-col md:flex-row border border-[#000000] border-opacity-25 max-w-full lg:max-w-[893px] py-10 rounded-[10px] mt-5 px-5">
-        <form className="w-full lg:w-[550px] xl:w-[893px]">
-          <div className="">
-            <div className="flex items-center gap-8 flex-col sm:flex-row">
-              <Select>
-                <SelectTrigger className="w-full sm:max-w-[273px] h-[52px] border border-[#491217] rounded-[10px]">
-                  <SelectValue placeholder="Service Type" />
-                </SelectTrigger>
-                <SelectContent className="bg-[#491217] text-white">
-                  <SelectItem
-                    value="ServiceTypeOne"
-                    className="hover:bg-slate-400 cursor-pointer"
-                  >
-                    Service Type One
-                  </SelectItem>
-                  <SelectItem
-                    value="ServiceTypeTwo"
-                    className="hover:bg-slate-400 cursor-pointer"
-                  >
-                    Service Type Two
-                  </SelectItem>
-                  <SelectItem
-                    value="ServiceTypeThree"
-                    className="hover:bg-slate-400 cursor-pointer"
-                  >
-                    Service Type Three
-                  </SelectItem>
-                </SelectContent>
-              </Select>
-              <Input
-                className="w-full sm:max-w-[273px] h-[52px] border border-[#491217] rounded-[10px]"
-                placeholder="Subject"
-              />
-            </div>
-            <div>
-              <Textarea
-                className="w-full max-w-full lg:w-[550px] xl:max-w-[579px] h-[145px] border border-[#491217] rounded-[10px] mt-5"
-                placeholder="Question?"
-              />
-            </div>
-            {/* Upload documents starts */}
-            <div className="flex flex-col gap-1 mt-5">
-              <div
-                {...getRootProps()}
-                className={`w-full max-w-full lg:max-w-[405px] h-[114px] rounded-[8px] border border-dashed border-spacing-2 border-[#000000] cursor-pointer flex items-center justify-center ${
-                  isDragActive ? "border-dashed border-black" : "border-dashed"
-                }`}
-              >
-                <input {...getInputProps()} />
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center gap-2">
-                    <div className="flex items-center gap-2 sm:gap-5 px-2">
-                      <img
-                        src="/images/cloud.svg"
-                        alt="cloud"
-                        className="w-[33px] h-[33px] sm:w-[44px] sm:h-[44px]"
-                      />
-                      <p className="text-base sm:text-lg text-[#000000]">
-                        {uploadedFiles.length > 0
-                          ? `${uploadedFiles[0].name} uploaded`
-                          : "Drop files to Upload, or"}
-                      </p>
-                      <p className="border-[#491217] text-sm sm:text-base border px-2 py-1 text-[#491217] rounded-[10px]">
-                        Browse
-                      </p>
+
+      <div className="w-full flex justify-between gap-4 lg:gap-8 border border-[#000000] border-opacity-25 p-6 rounded-[10px]">
+        <div className="flex flex-grow justify-center flex-col md:flex-row">
+          <form className="w-full ">
+            <div className="">
+              <div className="flex items-center gap-8 flex-col sm:flex-row">
+                <Select>
+                  <SelectTrigger className="w-full h-[52px] border border-[#491217] rounded-[10px]">
+                    <SelectValue placeholder="Service Type" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-[#491217] text-white">
+                    <SelectItem
+                      value="ServiceTypeOne"
+                      className="hover:bg-slate-400 cursor-pointer"
+                    >
+                      Service Type One
+                    </SelectItem>
+                    <SelectItem
+                      value="ServiceTypeTwo"
+                      className="hover:bg-slate-400 cursor-pointer"
+                    >
+                      Service Type Two
+                    </SelectItem>
+                    <SelectItem
+                      value="ServiceTypeThree"
+                      className="hover:bg-slate-400 cursor-pointer"
+                    >
+                      Service Type Three
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+                <Input
+                  className="w-full h-[52px] border border-[#491217] rounded-[10px]"
+                  placeholder="Subject"
+                />
+              </div>
+              <div>
+                <Textarea
+                  className="w-full h-[145px] border border-[#491217] rounded-[10px] mt-5"
+                  placeholder="Question?"
+                />
+              </div>
+              {/* Upload documents starts */}
+              <div className="flex flex-col gap-1 mt-5">
+                <div
+                  {...getRootProps()}
+                  className={`w-full max-w-full lg:max-w-[500px] h-[114px] rounded-[10px] border border-dashed border-spacing-2 border-[#000000] cursor-pointer flex items-center justify-center ${
+                    isDragActive ? "border-dashed border-black" : "border-dashed"
+                  }`}
+                >
+                  <input {...getInputProps()} />
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <div className="flex items-center gap-2 sm:gap-5 px-2">
+                        <img
+                          src="/images/cloud.svg"
+                          alt="cloud"
+                          className="w-[33px] h-[33px] sm:w-[44px] sm:h-[44px]"
+                        />
+                        <p className="text-base sm:text-lg text-[#000000]">
+                          {uploadedFiles.length > 0
+                            ? `${uploadedFiles[0].name} uploaded`
+                            : "Drop files to Upload, or"}
+                        </p>
+                        <p className="border-[#491217] text-sm sm:text-base border px-4 py-2 text-[#491217] rounded-[10px]">
+                          Browse
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
+              {/* Upload documents ends */}
+              <div className="mt-5 w-full flex items-center justify-center">
+                <button className="w-[205px] h-[55px] bg-[#491217] text-white rounded-[10px]">
+                  Submit
+                </button>
+              </div>
             </div>
-            {/* Upload documents ends */}
-            <div className="mt-5 flex items-center justify-center sm:justify-start sm:ml-48">
-              <button className="w-[205px] h-[55px] bg-[#491217] text-white rounded-[10px]">
-                Submit
-              </button>
-            </div>
+          </form>
+          <div className="hidden md:flex flex-col gap-3 ml-5">
+            <p className="text-2xl">Please Note.</p>
+            <p className="w-[200px] text-base flex flex-col gap-2">
+              <span>·Kindly Ensure the question you are about to ask is relevant to law
+              and avoid repetition of questions.</span>
+              <span>·Done tasks will be deleted in
+              10-20 days after review.</span>
+              <span>· If you post more than one question or
+              multiple questions, it will be regarded as full preparation request
+              and it will be charged accordingly.</span>
+            </p>
           </div>
-        </form>
-        <div className="hidden md:block ml-5">
-          <p className="text-2xl">Please Note.</p>
-          <p className="w-[190px] text-base">
-            ·Kindly Ensure the question you are about to ask is relevant to law
-            and avoid repetition of questions. ·Done tasks will be deleted in
-            10-20 days after review. · If you post more than one question or
-            multiple questions, it will be regarded as full preparation request
-            and it will be charged accordingly.
-          </p>
         </div>
       </div>
+
       <div className="block py-5 md:hidden">
         <p className="text-2xl">Please Note.</p>
         <p className="w-full text-base">
@@ -138,7 +144,7 @@ const PanelOfExperts = () => {
           and it will be charged accordingly.
         </p>
       </div>
-      <div className="w-full lg:max-w-[893px] my-5 overflow-x-auto">
+      <div className="w-full my-5 overflow-x-auto">
         <Table className="w-full">
           <TableHeader>
             <TableHead>ID</TableHead>
